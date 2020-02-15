0522325671ade09ed4bc9f6afb530a0921a7d715
Haylee <hayleezuba@gmail.com>
2020-02-15 15:36:03 -0500

    implemented drivetrain

--- src/main/java/frc/team1523/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/team1523/robot/subsystems/Drivetrain.java
@@ -1,11 +1,25 @@
 package frc.team1523.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Drivetrain extends SubsystemBase {
-    private final WPI_TalonFX m_leftMotor = new WPI_TalonFX(2);
-    private final WPI_TalonFX m_rightMotor = new WPI_TalonFX(3);
-    private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftMotor, m_rightMotor);
+    private final WPI_TalonFX leftfront = new WPI_TalonFX(2);
+    private final WPI_TalonFX rightrear = new WPI_TalonFX(3);
+    private final WPI_TalonSRX leftrear = new WPI_TalonSRX(4);
+    private final WPI_TalonSRX rightfront = new WPI_TalonSRX(5);
+
+    SpeedController leftmotors = new SpeedControllerGroup(leftfront, leftrear);
+    SpeedController rightmotors = new SpeedControllerGroup(rightfront, rightrear);
+    private final DifferentialDrive m_robotDrive = new DifferentialDrive(leftmotors, rightmotors);
+
+    public void drive(double xSpeed, double rotation){
+        m_robotDrive.arcadeDrive(xSpeed, rotation);
+    }
+
+
 }
