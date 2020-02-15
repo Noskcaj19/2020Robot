23b253129e95ceb867c534afd76ffafd0f946d89
Haylee <hayleezuba@gmail.com>
2020-02-15 15:55:25 -0500

    implemented shooter

--- src/main/java/frc/team1523/robot/subsystems/Shooter.java
+++ src/main/java/frc/team1523/robot/subsystems/Shooter.java
@@ -1,8 +1,17 @@
 package frc.team1523.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
 public class Shooter extends SubsystemBase {
+    private final CANSparkMax shooterMotor = new CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless);
+
+    public void setMotorSpeed(double speed){
+        shooterMotor.set(speed * 0.5);
+    }
+
 
 }
