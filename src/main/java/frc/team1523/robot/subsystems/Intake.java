f1695c1ddf0f73f91e502d53bf6c029c244f270f
Haylee <hayleezuba@gmail.com>
2020-02-15 15:35:29 -0500

    implemented drivetrain

--- src/main/java/frc/team1523/robot/subsystems/Intake.java
+++ src/main/java/frc/team1523/robot/subsystems/Intake.java
@@ -1,8 +1,23 @@
 package frc.team1523.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.SparkMax;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 
 public class Intake extends SubsystemBase {
+    CANSparkMax intake = new CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless);
+
+    public void setMotorSpeed(double speed){
+        intake.set(speed);
+
+
+
+    }
+
+
 
 }
