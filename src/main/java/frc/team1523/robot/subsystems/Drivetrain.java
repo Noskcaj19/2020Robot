1ee76688454b5c6dcaee29927f8289dde38a3462
Noskcaj <kcajdev@gmail.com>
2020-03-05 11:08:30 -0500

    Correct TalonSrx to TalonFrx

--- src/main/java/frc/team1523/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/team1523/robot/subsystems/Drivetrain.java
@@ -2,7 +2,6 @@ package frc.team1523.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -11,8 +10,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Drivetrain extends SubsystemBase {
     private final WPI_TalonFX leftFront = new WPI_TalonFX(2);
     private final WPI_TalonFX rightRear = new WPI_TalonFX(3);
-    private final WPI_TalonSRX leftRear = new WPI_TalonSRX(4);
-    private final WPI_TalonSRX rightFront = new WPI_TalonSRX(5);
+    private final WPI_TalonFX leftRear = new WPI_TalonFX(4);
+    private final WPI_TalonFX rightFront = new WPI_TalonFX(5);
 
     SpeedController leftMotors = new SpeedControllerGroup(leftFront, leftRear);
     SpeedController rightMotors = new SpeedControllerGroup(rightFront, rightRear);
