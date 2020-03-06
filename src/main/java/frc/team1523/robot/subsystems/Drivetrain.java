b906b17b509a29e07a992282faa77bd40853542d
Noskcaj <kcajdev@gmail.com>
2020-03-06 15:56:52 -0500

    Add navx

--- src/main/java/frc/team1523/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/team1523/robot/subsystems/Drivetrain.java
@@ -2,6 +2,7 @@ package frc.team1523.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -12,6 +13,7 @@ public class Drivetrain extends SubsystemBase {
     private final WPI_TalonFX rightRear = new WPI_TalonFX(3);
     private final WPI_TalonFX leftRear = new WPI_TalonFX(4);
     private final WPI_TalonFX rightFront = new WPI_TalonFX(5);
+    private final AHRS navx = new AHRS();
 
     SpeedController leftMotors = new SpeedControllerGroup(leftFront, leftRear);
     SpeedController rightMotors = new SpeedControllerGroup(rightFront, rightRear);
@@ -26,6 +28,14 @@ public class Drivetrain extends SubsystemBase {
         rightRear.setNeutralMode(NeutralMode.Brake);
         leftRear.setNeutralMode(NeutralMode.Brake);
         rightFront.setNeutralMode(NeutralMode.Brake);
+    }
+
+    public double getAngle() {
+        return navx.getAngle();
+    }
+
+    public void zeroSensors() {
+        navx.zeroYaw();
     }
 
     public void drive(double xSpeed, double rotation) {
